fae9ebbdc125187992b743b53351d81814b6440c
tsktsukamoto <wataru.tsukamoto@tokai-soft.co.jp>
2021-05-20 12:07:48 +0900

    “サーバからのsrc取得テスト”

---
+++ files/App/testApp/main.cpp
@@ -0,0 +1,18 @@
+
+#include <QtPlugin>
+#include <QPluginLoader>
+#include <QDir>
+#include <QTimer>
+#include <QDebug>
+#include <QThread>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello");
+    return 0;
+}
+
